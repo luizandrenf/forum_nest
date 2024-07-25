1d3d605963372e9b35ea545096be58d680d51e33
luizandrenf <luizandrenf@gmail.com>
2024-07-24 20:55:16 -0400

    feat: creating comments and attachments on prisma schema

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -4,13 +4,13 @@ export abstract class Entity<Props> {
   private _id: UniqueEntityId
   protected props: Props
 
-  get id() {
-    return this._id
-  }
-
   protected constructor(props: Props, id?: UniqueEntityId) {
     this.props = props
     this._id = id ?? new UniqueEntityId(undefined)
+  }
+
+  get id() {
+    return this._id
   }
 
   public equals(entity: Entity<unknown>) {
